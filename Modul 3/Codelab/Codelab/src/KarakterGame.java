b0c25161ea585b20e58ba8033690f8fba1751cb9
A_Yossua <yossuagung2@gmail.com>
2025-04-12 20:16:06 +0700

    Add files via upload

---
+++ Modul 3/Codelab/Codelab/src/KarakterGame.java
@@ -0,0 +1,62 @@
+<<<<<<< HEAD
+public class KarakterGame {
+    private String nama;
+    private int kesehatan;
+
+    public KarakterGame(String nama, int kesehatan) {
+        this.nama = nama;
+        this.kesehatan = kesehatan;
+    }
+    public String getNama(){
+        return nama;
+    }
+    //setter nama
+    public void setNama (String nama){
+        this.nama = nama;
+    }
+    //setter kesehatan
+    public void setKesehatan(int kesehatan) {
+        this.kesehatan = kesehatan;
+    }
+
+    //getter kesehatan
+    public int getKesehatan (){
+        return kesehatan;
+    }
+    void serang(KarakterGame target){
+        System.out.println(nama + "Menyerang " + target.getNama()+"!");
+    }
+
+}
+=======
+public class KarakterGame {
+    private String nama;
+    private int kesehatan;
+
+    public KarakterGame(String nama, int kesehatan) {
+        this.nama = nama;
+        this.kesehatan = kesehatan;
+    }
+    public String getNama(){
+        return nama;
+    }
+    //setter nama
+    public void setNama (String nama){
+        this.nama = nama;
+    }
+    //setter kesehatan
+    public void setKesehatan(int kesehatan) {
+        this.kesehatan = kesehatan;
+    }
+
+    //getter kesehatan
+    public int getKesehatan (){
+        return kesehatan;
+    }
+    void serang(KarakterGame target){
+        System.out.println(nama + "Menyerang " + target.getNama()+"!");
+    }
+
+}
+
+>>>>>>> 30e5a3c402f28df47355da90d596622effc86615
---
+++ Modul 3/Codelab/Codelab/src/Musuh.java
@@ -0,0 +1,27 @@
+<<<<<<< HEAD
+class Musuh extends KarakterGame {
+    public Musuh(String nama, int kesehatan){
+        super(nama, kesehatan);
+    }
+
+    @Override
+    void serang(KarakterGame target) {
+        System.out.println(getNama() + " menyerang " + target.getNama() + " menggunakan sihir!");
+        target.setKesehatan(target.getKesehatan() - 15);
+        System.out.println("Kesehatan " + target.getNama() + " sekarang " + target.getKesehatan());
+    }
+}
+=======
+class Musuh extends KarakterGame {
+    public Musuh(String nama, int kesehatan){
+        super(nama, kesehatan);
+    }
+
+    @Override
+    void serang(KarakterGame target) {
+        System.out.println(getNama() + " menyerang " + target.getNama() + " menggunakan sihir!");
+        target.setKesehatan(target.getKesehatan() - 15);
+        System.out.println("Kesehatan " + target.getNama() + " sekarang " + target.getKesehatan());
+    }
+}
+>>>>>>> 30e5a3c402f28df47355da90d596622effc86615
